2540c629616c473d549a3f600cec07bc7d641de0
Proger <lvvasilenko1977@mail.ru>
2026-04-24 21:17:29 +0400

    Добавили удобное меню

--- zanatie_4_5_2.py
+++ zanatie_4_5_2.py
@@ -24,10 +24,39 @@ def coin_flip():
 
 def gess_number():
    secret = random.randint(1, 10)
-   print('Угадай число от 1 до 10')
+   gess_number = input('Угадай число от 1 до 10: > ')
    if gess_number == secret:
       print('Круто! Ты угадал.')
    else:      
       print(f'Не угадал! Было число {secret}')
 
+tasks = []
 
+def add_tasks():
+   task = input('Что нужно сделать? ')
+   tasks.append(task)
+   print(f'Задача добавлена: {task}')
+   
+def main():
+   while True:
+      print('\n=== Меню проекта ===')
+      print('1. Приветствие')
+      print('2. Цитата дня')
+      print('3. Орел / Решка')
+      print('4. Угадай число')
+      print('5. Добавить задачу')
+      print('6. Выход')
+      
+      choice = input('Выбери номер: >>> ')
+
+      if choice == '1': greeting()
+      elif choice == '2': motivation_quote()
+      elif choice == '3': coin_flip()
+      elif choice == '4': gess_number()
+      elif choice == '5': add_tasks()
+      elif choice == '6':
+         print('Пока, команда!')
+         break
+      else: print('Ошибка: введите число (1-6)')
+         
+main()         
